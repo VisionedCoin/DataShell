fe207bc046f7551b742d48a6b3e79725f53e37f4
Juan Carlos Garcia <jcgarciamx126@gmail.com>
2020-11-26 17:43:55 -0600

    calificaciones juanca

---
+++ algoritmo.c
@@ -0,0 +1,15 @@
+//
+//  algoritmo.c
+//  
+//
+//  Created by Juan Carlos Garcia on 26/11/20.
+//
+
+#include "algoritmo.h"
+
+
+void entrenar(){
+    
+    //aqui va el algoritmo para entrenar al sistema de recomendaciones
+    
+}
---
+++ algoritmo.h
@@ -0,0 +1,39 @@
+//
+//  algoritmo.h
+//  
+//
+//  Created by Juan Carlos Garcia on 26/11/20.
+//
+
+#ifndef algoritmo_h
+#define algoritmo_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef algoritmo_IMPORT
+    #define EXTERN
+#else
+    #define EXTERN extern
+#endif
+
+
+/*
+ *  la funcion entrenar se utiliza al inicio para entrenar al algoritmo de recomendaciones
+ *
+ *@params
+ *
+ *
+ *@returns
+ *
+ */
+EXTERN void entrenar();
+
+
+
+
+
+#endif /* algoritmo_h */
+
+
