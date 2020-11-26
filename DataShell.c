203adee3345eeb3f2ef60b0183c2241022497c98
David <davidemmanuelpa@gmail.com>
2020-11-26 16:17:28 -0600

    Cambio de un error de los parametros

--- DataShell.c
+++ DataShell.c
@@ -51,7 +51,8 @@ fclose(archivo);
 }
 return i;
 }
-void DataShell_leer_matriz(char nombre, int *matriz[], int fil, int col)
+
+void DataShell_leer_matriz(char nombre[], int *matriz[], int fil, int col)
 {
         FILE *archivo=NULL;
         int i=0, j=0;
