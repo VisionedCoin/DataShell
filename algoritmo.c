5c4a26a994519fd6dcea030bd54183ec37ed73ef
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-11-29 19:21:50 -0600

    Update algoritmo.c

--- algoritmo.c
+++ algoritmo.c
@@ -13,3 +13,14 @@ void entrenar(){
     //aqui va el algoritmo para entrenar al sistema de recomendaciones
     
 }
+
+int ProductPunto(demovie *Peli11, demovie *Peli2)
+{
+    producto1=Peli1.accion*Peli2.accion+Peli1.comedia*Peli2.comedia+Peli1.romance*Peli2.romance;
+    Producto2=Peli1.terror*Peli2.terror+Peli1.ficcion*Peli2.ficcion+Peli1.drama*Peli2.drama;
+    Producto3=Peli1.historia*Peli2.historia+Peli1.documental*Peli2.documental+Peli1.arte*Peli2.arte+Peli1.animada*Peli2.animada;
+    
+    Final=producto1+producto2+producto3;
+    return Final1;
+    
+}
