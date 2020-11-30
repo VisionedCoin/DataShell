f26fee48efd1228afacf067044db7f90f47c1498
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-11-29 19:40:22 -0600

    Update algoritmo.c

--- algoritmo.c
+++ algoritmo.c
@@ -14,13 +14,14 @@ void entrenar(){
     
 }
 
-int ProductPunto(demovie *Peli11, demovie *Peli2)
+double ProductPunto(demovie *Peli11, demovie *Peli2)
 {
+    double producto1, producto2, producto3, Final;
     producto1=Peli1.accion*Peli2.accion+Peli1.comedia*Peli2.comedia+Peli1.romance*Peli2.romance;
-    Producto2=Peli1.terror*Peli2.terror+Peli1.ficcion*Peli2.ficcion+Peli1.drama*Peli2.drama;
-    Producto3=Peli1.historia*Peli2.historia+Peli1.documental*Peli2.documental+Peli1.arte*Peli2.arte+Peli1.animada*Peli2.animada;
+    producto2=Peli1.terror*Peli2.terror+Peli1.ficcion*Peli2.ficcion+Peli1.drama*Peli2.drama;
+    producto3=Peli1.historia*Peli2.historia+Peli1.documental*Peli2.documental+Peli1.arte*Peli2.arte+Peli1.animada*Peli2.animada;
     
     Final=producto1+producto2+producto3;
-    return Final1;
+    return Final;
     
 }
