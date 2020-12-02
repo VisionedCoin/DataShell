698d20bbf0d52f306b0db134873cfd75d85457ff
David <davidemmanuelpa@gmail.com>
2020-12-02 13:41:50 -0600

    Funcion Recomendar amigo

--- main.c
+++ main.c
@@ -10,8 +10,9 @@
 #include "DataShell.h"
 #include "Guardar.h"
 int Recomendar_pelicula(param Usu, param movies[], double matriz[10][30], int numPelis, int nuusu);
+int Recomendar_amigo(param usuarios[], int numUsuarios, int numUs);
 int main(void){
-    int numPelis, numUsuarios, opcion, x, numUs;
+    int numPelis, numUsuarios, opcion, op, x, R,numUs;
     double matriz[10][30];
     param usuarios[10],pelis[30];
     
@@ -28,58 +29,88 @@ int main(void){
     printf("Cuál usuario es usted: ");
     scanf("%d",&numUs);
     getchar();
-    
-    
-    
+
     entrenar(usuarios, pelis, matriz, numUsuarios, numPelis );
     //Menu:
     do{
-        system(clear);
-        printf(MENU:\n\t1. Recomendacion de peliculas\n\t2. Peliculas populares\n\t3. Peliculas similares\n\t4. Recomendar amigos\n\t9. Salir\n\n);
+        //system("clear");
+        printf("MENU:\n\t1. Recomendacion de peliculas\n\t2. Peliculas populares\n\t3. Peliculas similares\n\t4. Recomendar amigos\n\t9. Salir\n\n");
         scanf("%d",&opcion);
-        switch(opcion):
+	getchar();
+	
+        switch(opcion)
         {
             case 1:
-            
+	R=Recomendar_pelicula(usuarios[numUs], pelis, matriz, numPelis, numUs);
+	printf("A %s le recomendamos %s\n", usuarios[numUs].nombre, pelis[R].nombre);
+	//printf("¿Quieres verla? 1. Si 2. No: ");
+	getchar();
             break;
             case 2:
+
             break;
             case 3:
+
             break;
-            case4:
+            case 4:
+	R=Recomendar_amigo(usuarios, numUsuarios, numUs);
+	printf("%s puede ser tu amigo :)", usuarios[R].nombre);
+	getchar();
             break;
             case 9:
-            break; 
+	printf("Adios\n");
+            break;
             default:
             break;
         }
             
         
         
-    }while(opcion!=9)
-    
+    }while(opcion!=9);
+/*
 for(i=0;i<numUsuarios;i++)
 {
 R=Recomendar_pelicula(usuarios[i], pelis, matriz, numPelis, i);
-printf("A %s le recomendamos %s", usuarios[i].nombre, pelis[R].nombre);
-} 
+printf("A %s le recomendamos %s\n", usuarios[i].nombre, pelis[R].nombre);
+} */
    Guardar_datos("DatosUsuarios.csv", usuarios, numUsuarios);
-    
 }
 
-int Recomendar_pelicula(param usu, param movies[], double matriz[5][30], int numpelis, int nuusu)
+int Recomendar_pelicula(param usu, param movies[], double matriz[10][30], int numpelis, int nuusu)
 {
 	int i, es;
-	double re=-100,Pp; 
+	double re=-100,Pp;
 	for(i=0;i<numpelis;i++)
 {
 if(matriz[nuusu][i]==0)
 {
 Pp=ProductPunto(usu, movies[i]);
 if(Pp>re)
+{
 re=Pp;
 es=i;
 }
+}
+}
+return es;
+}
+
+int Recomendar_amigo(param usuarios[], int numUsuarios, int numUs)
+{
+	int i, es;
+	double re=-1000, Pp;
+	for(i=0;i<numUsuarios;i++)
+	{
+	if(i!=numUs)
+		{
+		Pp=ProductPunto(usuarios[numUs], usuarios[i]);
+			if(Pp>re)
+				{
+				re=Pp;
+				es=i;
+				}
+		}
+
 }
 return es;
 }
