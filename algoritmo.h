09c227522896cf13b7dd09e9e1b017173c995338
MirnaCardona <69806001+MirnaCardona@users.noreply.github.com>
2020-12-02 13:07:52 -0600

    Update algoritmo.h
    comentarios agregados

--- algoritmo.h
+++ algoritmo.h
@@ -20,30 +20,43 @@
     #define EXTERN extern
 #endif
 
-
-
-
-
-
-
-
-
-
 /*
  *  la funcion entrenar se utiliza al inicio para entrenar al algoritmo de recomendaciones
  *
  *@params
  *
- *
+ *  usuarios (param []):
+ *      arreglo de usuarios
+ *  movies (param []):
+ *      arreglo de peliculas
+ *  matriz (double [][]):
+ *      matriz de calificaciones
+ *  numUsuarios (int):
+ *      numero de usuarios
+ *  numMovies (int):
+ *      numero de peliculas
  *@returns
+ *  void
  *
  */
+
 EXTERN void entrenar(param usuarios[],param movies[], double matriz[5][30], int numUsuarios, int numMovies);
+
+/*
+ *  la funcion ProcuctPunto se utiliza para obtener el producto punto de dos peliculas
+ *
+ *@params
+ *
+ *  Peli1 (param):
+ *      estructura pelicula 1
+ *  Peli2 (param):
+ *      estructura pelicula 2
+ *@returns
+ *  double (resultado del producto punto)
+ *
+ */
+
 EXTERN double ProductPunto(param Peli11, param Peli2);
-
-
-
-
 
 #endif /* algoritmo_h */
 
